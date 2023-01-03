d85314864f3a223cd64e82c46cb6d93f41d4fb35
jieunhanme <jieun.han@brique.co.kr>
2023-01-03 22:38:27 +0900

    feat : google login 기능 추가

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,17 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 const Home = () => {
-  return <div>HOME PAGE</div>;
+  const { data } = useSession();
+
+  return (
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign In</button>
+      )}
+    </div>
+  );
 };
 
 export default Home;
