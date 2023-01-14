60c3d42445e4e618737e221ad2a7dcdbb1d6cc88
jieunhanme <jieun93.h@gmail.com>
2023-01-14 23:36:41 +0900

    feat : [Backend] user resolver  > createUsername 함수 작성중

--- backend/src/graphql/resolvers/user.ts
+++ backend/src/graphql/resolvers/user.ts
@@ -1,9 +1,39 @@
+import { CreateUsernameResponse, GraphQLContext } from "../../util/types";
+
 const resolvers = {
   Query: {
     searchUsers: (username: String) => {},
   },
   Mutation: {
-    createUsername: (username: String) => {},
+    createUsername: async (
+      _: any,
+      args: { username: string },
+      context: GraphQLContext
+    ) => {
+      const { username } = args;
+      const { session, prisma } = context;
+      console.log("🚀 ~ file: user.ts:15 ~ session", session);
+
+      if (!session?.user) {
+        return {
+          error: "Not Authorized",
+        };
+      }
+
+      const { id } = session.user;
+
+      try {
+        /**
+         * Check that username is not taken
+         */
+        console.log("TEST", id);
+      } catch (error) {
+        console.log("[ERROR] createUsername : ", error);
+        return {
+          error: error?.message,
+        };
+      }
+    },
   },
 };
 
---
+++ backend/src/util/types.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { ISODateString } from "next-auth";
+
+export interface GraphQLContext {
+  session: Session | null;
+  prisma: PrismaClient;
+  //pubsub
+}
+
+/**
+ * Users
+ */
+
+export interface Session {
+  user: User;
+  expires: ISODateString;
+}
+export interface User {
+  id: string;
+  username: string;
+}
+
+export interface CreateUsernameResponse {
+  success?: boolean;
+  error?: string;
+}
