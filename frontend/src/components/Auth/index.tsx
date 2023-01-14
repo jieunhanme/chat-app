982257e7d1e3c19e6d64dbc29254fdc53b1f2ce0
jieunhanme <jieun93.h@gmail.com>
2023-01-14 23:37:55 +0900

    feat : [Frontend] 유저 닉네임 생성 api 연동중

--- frontend/src/components/Auth/index.tsx
+++ frontend/src/components/Auth/index.tsx
@@ -1,7 +1,11 @@
+import { useMutation } from "@apollo/client";
 import { Button, Center, Image, Input, Stack, Text } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
+
+import UserOperations from "@graphQL/user";
+import { CreateUsernameData, CreateUserNameVariables } from "@util/types";
 
 interface IAuthProps {
   session: Session | null;
@@ -9,15 +13,32 @@ interface IAuthProps {
 }
 
 const Auth = ({ session, reloadSession }: IAuthProps) => {
+  console.log("🚀 ~ file: index.tsx:16 ~ Auth ~ session", session);
   const [username, setUsername] = useState<string>("");
+
+  /**
+   * TypeScript를 위한 Type정의를 추가해야 좋은 코드!
+   * graphql/user에서 정의한 Type은 grqphql을 위한 정의이기 때문!!
+   * 추가 설명
+   * @createUsername : biz에서 해당 graphql을 사용하는 function명
+   * @CreateUsernameData : Response의 Type
+   * @CreateUserNameVariables : Request의 Type
+   */
+  const [createUsername, { data, loading, error }] = useMutation<
+    CreateUsernameData,
+    CreateUserNameVariables
+  >(UserOperations.Mutations.createUsername);
 
   const onSubmit = async () => {
     try {
-      // TODO createUsername Mutation to send our username to the GraphQL API
+      if (!username) return;
+      await createUsername({ variables: { username } });
     } catch (error) {
       console.log("onSubmit error", error);
     }
   };
+
+  console.log("HERE IS DATA : ", data, loading, error);
 
   return (
     <Center height="100vh" border="1px solid gray">
@@ -28,7 +49,7 @@ const Auth = ({ session, reloadSession }: IAuthProps) => {
             <Input
               placeholder="Enter a username"
               value={username}
-              onChange={(event) => setUsername(event.target.value)}
+              onChange={(event) => setUsername(event.target.value.trim())}
             />
             <Button width="100%" onClick={onSubmit}>
               Save
---
+++ frontend/src/graphql/operations/user.ts
@@ -0,0 +1,18 @@
+import { gql } from "@apollo/client";
+
+const User = {
+  Queries: {},
+  Mutations: {
+    createUsername: gql`
+      mutation CreateUsername($username: String!) {
+        createUsername(username: $username) {
+          success
+          error
+        }
+      }
+    `,
+  },
+  Subscriptions: {},
+};
+
+export default User;
