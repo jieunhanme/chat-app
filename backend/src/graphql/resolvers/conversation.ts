2c640662556568a7fbaaf1cda995594d2ec1dd09
jieunhanme <jieun93.h@gmail.com>
2023-01-18 16:01:33 +0900

    feat : [Backend] conversation prisma.create 기능 추가

--- backend/src/graphql/resolvers/conversation.ts
+++ backend/src/graphql/resolvers/conversation.ts
@@ -11,8 +11,50 @@ const resolvers = {
       const { participantIds } = args;
       const { session, prisma } = context;
 
+      if (!session?.user) {
+        throw new GraphQLError("Not Authorized");
+      }
+
+      const {
+        user: { id: userId },
+      } = session;
+
       try {
-        // await prisma.conversation.update({});
+        const conversation = await prisma.conversation.create({
+          data: {
+            participants: {
+              createMany: {
+                data: participantIds.map((id) => ({
+                  userId: id,
+                  hasSeenLatestMessage: id === userId,
+                })),
+              },
+            },
+          },
+          include: {
+            participants: {
+              include: {
+                user: {
+                  select: {
+                    id: true,
+                    username: true,
+                  },
+                },
+              },
+            },
+            latestMessage: {
+              include: {
+                sender: {
+                  select: {
+                    id: true,
+                    username: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+
         return { conversationId: "" };
       } catch (error: any) {
         console.log("[ERROR] createConversation : ", error);
