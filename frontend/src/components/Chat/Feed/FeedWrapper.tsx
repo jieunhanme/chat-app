47c63d1c28dc674e403c568be44b219c6f21c603
jieunhanme <jieun93.h@gmail.com>
2023-01-19 12:57:02 +0900

    feat : [Frontend] 채팅방 선택 유무에 따른 화면 출력 적용(media query적용)

--- frontend/src/components/Chat/Feed/FeedWrapper.tsx
+++ frontend/src/components/Chat/Feed/FeedWrapper.tsx
@@ -1,11 +1,29 @@
+import { Flex } from "@chakra-ui/react";
 import { Session } from "next-auth";
+import { useRouter } from "next/router";
 
 interface FeedWrapperProps {
   session: Session;
 }
 
 const FeedWrapper = ({ session }: FeedWrapperProps) => {
-  return <div>FeedWrapper</div>;
+  const router = useRouter();
+
+  const { conversationId } = router.query;
+
+  return (
+    <Flex
+      display={{ base: conversationId ? "flex" : "none", md: "flex" }}
+      direction="column"
+      width="100%"
+    >
+      {conversationId ? (
+        <Flex>{conversationId}</Flex>
+      ) : (
+        <div> No Conversation Selected</div>
+      )}
+    </Flex>
+  );
 };
 
 export default FeedWrapper;
