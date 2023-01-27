72ed7df14791c06e8a132f7fbfed726a1936080a
jieunhanme <jieun93.h@gmail.com>
2023-01-28 00:45:04 +0900

    feat : [Frontend] sendMessage api연동

--- frontend/src/components/Chat/Feed/Messages/Input.tsx
+++ frontend/src/components/Chat/Feed/Messages/Input.tsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import { Box, Input } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { toast } from "react-hot-toast";
+import { useMutation } from "@apollo/client";
+import { ObjectID } from "bson";
+
+import MessageOperations from "@graphQL/message";
+import { SendMessageArguments } from "../../../../../../backend/src/util/types";
 
 interface MessageInputProps {
   session: Session;
@@ -11,11 +16,36 @@ interface MessageInputProps {
 const MessageInput = ({ session, conversationId }: MessageInputProps) => {
   const [messageBody, setMessageBody] = useState<string>("");
 
+  const [sendMessage] = useMutation<
+    { sendMessage: boolean },
+    SendMessageArguments
+  >(MessageOperations.Mutations.sendMessage);
+
+  const {
+    user: { id: userId },
+  } = session;
+
   const onSendMessage = async (event: React.FormEvent) => {
     event.preventDefault();
 
     try {
-      // call snedMessage Mutation
+      const messageId = new ObjectID().toString();
+      const newMessage: SendMessageArguments = {
+        id: messageId,
+        conversationId: conversationId,
+        senderId: userId,
+        body: messageBody,
+      };
+
+      const { data, errors } = await sendMessage({
+        variables: newMessage,
+      });
+
+      if (!data?.sendMessage || errors) {
+        throw new Error("Failed to send message");
+      }
+
+      setMessageBody("");
     } catch (error: any) {
       console.log("[ERROR] onSendMessage", error);
       toast.error(error?.message);
