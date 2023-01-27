36dfe96f08f5637eb4cb1186bbeee1afd18c73f0
jieunhanme <jieun93.h@gmail.com>
2023-01-28 01:50:39 +0900

    feat : [Frontend] 날짜 형식 공통 함수 추가

--- frontend/src/util/functions.ts
+++ frontend/src/util/functions.ts
@@ -1,3 +1,6 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ko } from "date-fns/locale";
+
 import { ParticipantPopulated } from "../../../backend/src/util/types";
 
 /**
@@ -15,4 +18,28 @@ export const formatUsernames = (
     .map((participant) => participant.user.username);
 
   return usernames.join(", ");
+};
+
+/**
+ * date-fns로 날짜 형식 뽑기
+ */
+export const formatDate = (date: string, specific = false) => {
+  const d = new Date(date);
+  if (specific) {
+    return format(d, "PPP EEE a hh:mm", { locale: ko });
+  }
+
+  const now = Date.now();
+  const diff = (now - d.getTime()) / 1000; // 현재 시간과의 차이(초)
+  if (diff < 60 * 1) {
+    // 1분 미만일땐 방금 전 표기
+    return "방금 전";
+  }
+  if (diff < 60 * 60 * 24 * 3) {
+    // 3일 미만일땐 시간차이 출력(몇시간 전, 몇일 전)
+    return formatDistanceToNow(d, { addSuffix: true, locale: ko });
+  }
+  //  PPP는 날짜, EEE는 요일, p는 시간 "PPP EEE p"
+  // addSuffix : 접미사 붙이기 ( ...전, ...후 )
+  return format(d, "yyyy.MM.dd", { locale: ko }); // 날짜 포맷
 };
